1d3d546dcc3cd046d1749a13e4af1d90ca59319f
devfer <ferkki@hotmail.com>
2020-01-10 22:32:22 -0400

    build

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  heroesUrl: "https://heroesapp-3272e.firebaseio.com/heroes.json",
-  heroeUrl: "https://heroesapp-3272e.firebaseio.com/heroes/"
-};
